32442fda893cf2ea808a81af68f29610abc1933f
Tuuben <tobbe.helsing@gmail.com>
2019-11-26 15:23:16 +0200

    booking service validation check

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 import { combineCollectionSnapshot, combineDocument } from './../helper';
 import { Booking } from './booking.model';
@@ -16,6 +16,15 @@ export class BookingService {
   }
 
   async createBooking(dogId: string, userId: string) {
+    const [userHasActiveBooking, dogIsBooked] = await Promise.all([
+      this.userHasActiveBooking(userId),
+      this.getIsDogBooked(dogId),
+    ]);
+
+    if (userHasActiveBooking || dogIsBooked) {
+      throw new HttpException('Already booked.', HttpStatus.BAD_REQUEST);
+    }
+
     const booking = {
       dogId,
       userId,
@@ -86,9 +95,7 @@ export class BookingService {
 
     const bookings = combineCollectionSnapshot(snapshot) as Booking[];
 
-    const activeBooking = bookings.filter(
-      b => !b.returnedAt && !b.pendingReturn,
-    )[0];
+    const activeBooking = bookings.filter(b => !!b.active)[0];
 
     return !!activeBooking;
   }
@@ -98,12 +105,23 @@ export class BookingService {
       .firestore()
       .collection('bookings')
       .where('dogId', '==', dogId)
+      .where('active', '==', true)
       .get();
 
     const bookings = combineCollectionSnapshot(snapshot) as Booking[];
 
-    return (
-      bookings && !!bookings.filter(b => !b.returnedAt && !b.pendingReturn)
-    );
+    return bookings && !!bookings.length;
+  }
+
+  async userHasActiveBooking(userId: string) {
+    const snapshot = await admin
+      .firestore()
+      .collection('bookings')
+      .where('userId', '==', userId)
+      .where('active', '==', true)
+      .get();
+
+    const booking = combineCollectionSnapshot(snapshot)[0];
+    return !!booking;
   }
 }
